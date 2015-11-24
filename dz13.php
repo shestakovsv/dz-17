6aa560c4fea3dbded11c6b175484e0cbb8973731
Shestakov Sergey <shestakovpost@gmail.com>
2015-11-25 09:55:53 +1000

    delete

--- dz13.php
+++ dz13.php
@@ -8,6 +8,9 @@ header('Content-type: text/html; charset=utf-8');
     console.log("Меня зовут ", name);
     console.log("Мне ", age, " лет");
 
+//    delete name;
+//    delete age;
+
     var SITY = "Komsomolsk";
     if (SITY !== undefined) {
         console.log("я живу в городе ", SITY);
@@ -16,22 +19,24 @@ header('Content-type: text/html; charset=utf-8');
     }
 
     var book = new Array();
-    book["title"] = "Rebus Gaala";
-    book["autor"] = "Panov";
-    book["pages"] = 333;
+    book.title = "Rebus Gaala";
+    book.autor = "Panov";
+    book.pages = 333;
 
-    console.log("Недавно я прочитал книгу ", book['title'], " написанную автором ", book["autor"], " я этой книге ", book["pages"], " страниц");
+
+
+    console.log("Недавно я прочитал книгу ", book.title, " написанную автором ", book.autor, " я этой книге ", book.pages, " страниц");
 
     var book2 = new Array();
-    book2["title"] = "Everest";
-    book2["autor"] = "Anatoliy Bukreev";
-    book2["pages"] = 500;
+    book2.title = "Everest";
+    book2.autor = "Anatoliy Bukreev";
+    book2.pages = 500;
 
     var books = new Array();
-    books["book"] = book;
-    books["book2"] = book2;
+    books.book = book;
+    books.book2 = book2;
 
-    console.log("Недавно я прочитал книги ", books["book"]['title'], " и ", books["book2"]['title'], ", написанные соответственно авторами ", books["book"]["autor"], "  и ", books["book2"]["autor"], " , я осилил в сумме ", (books["book"]["pages"]+books["book2"]["pages"]), " , не ожидал от себя подобного");
+    console.log("Недавно я прочитал книги ", books.book.title, " и ", books.book2.title, ", написанные соответственно авторами ", books.book.autor, "  и ", books.book2.autor, " , я осилил в сумме ", (books.book.pages + books.book2.pages), " , не ожидал от себя подобного");
 
 
 </script>
