764cae5c9a3b01d064dac1d9ec92a197ff80e44d
Shestakov Sergey <shestakovpost@gmail.com>
2015-12-12 17:57:59 +1000

    callback

--- del.js
+++ del.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
     $('button.btn').on('click', function () {
         var tr = $(this).closest('tr');
         var id = tr.children('td:first').html();
-        $('#container').load('ajax.php?id_del=' + id);
-       tr.fadeOut('slow', function () {
-            tr.remove();
-       });
+        $('#container').load('ajax.php?id_del=' + id, function () {
+            tr.fadeOut('slow', function () {
+                tr.remove();
+            });
+        });
     });
 });
