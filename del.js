b1db84b9dd3814574a478d4bd46d77c6aae443a0
Shestakov Sergey <shestakovpost@gmail.com>
2015-12-12 22:13:47 +1000

    index

--- del.js
+++ del.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('button.btn').on('click', function () {
         var tr = $(this).closest('tr');
         var id = tr.children('td:first').html();
-        $('#container').load('ajax.php?id_del=' + id, function () {
+        $('#container').load('index.php?id_del=' + id, function () {
             tr.fadeOut('slow', function () {
                 tr.remove();
             });
